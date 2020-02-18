b4e0079ef549966a532f126409ebfcad2aec9d97
Devin Bayly <baylyd@email.arizona.edu>
2020-02-18 12:13:25 -0700

    first commit of converted interface

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Top from "./Top.svelte";
+  import Middle from "./Middle.svelte";
+  import Bottom from "./Bottom.svelte";
+</script>
+
+<Top/>
+
+<Middle />
+
+<Bottom/>
---
+++ src/Tile.svelte
@@ -0,0 +1,28 @@
+<style>
+  .even {
+    justify-content: space-evenly;
+    flex-wrap: wrap;
+  }
+  .tile {
+    background-color: #eee;
+    padding: 0% !important;
+    margin: 2%;
+    width: 80px;
+    height: 80px;
+    display: inline-block;
+  }
+  .centerer {
+    display: flex;
+    justify-content: center;
+    align-content: center;
+    height: 100%;
+  }
+  .title {
+    word-break: break-all;
+    height: 80%;
+    width: 80%;
+    margin: auto auto;
+    text-align: center;
+    font-size: 10px;
+  }
+</style>
