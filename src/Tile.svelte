16736ffadf7a8078255ddc742f7470831cea5365
DevinBayly <baylyd@email.arizona.edu>
2021-01-05 21:16:21 +0000

    make alec's changes

--- src/Tile.svelte
+++ src/Tile.svelte
@@ -1,6 +1,24 @@
 <script>
   import {  replace } from "./transition.js";
+import {tweened} from 'svelte/motion'
+import {cubicOut} from 'svelte/easing'
   export let specOb;
+  let current 
+  const scroll = tweened(0,{
+    duration:800,
+    easing:cubicOut,
+  })
+  let active = false
+scroll.subscribe(value=> {
+if (active) {
+console.log(value)
+window.scrollTo(0,value*window.innerHeight + current-current*value)
+}
+if (value == 1) {
+active = false
+scroll.set(0)
+}
+})
 </script>
 
 <style>
@@ -23,6 +41,9 @@
 
 <div class="tile"  on:click={() => {
   replace(specOb)
+  current = window.scrollY
+  active = true
+  scroll.set(1.0)
 }}>
   <div class="titleHolder">
     <p class="title">{specOb.name}</p>
--- src/transition.js
+++ src/transition.js
@@ -1,6 +1,11 @@
 import Specific from "./Specific.svelte"
 
 export let replace = (specOb) => {
+  // remove all existing bottom categories
+  for (let c of document.querySelector("#bottom").children) {
+    c.remove()
+  }
+
     // create single page entity
     new Specific({
         target: document.querySelector("#bottom"),
@@ -8,4 +13,6 @@ export let replace = (specOb) => {
             ob: specOb
         }
     })
+  // use a tweened to update page
+
 }
